2ebb3443a4ae1332b1e73c526bfa8dff7b9b9aba
Martinho Tavares <martinho.tavares@ua.pt>
2022-09-25 10:27:30 +0100

    Added a safeguard to prevent colliding on the map's edges and breaking the simulator

--- mainRob.py
+++ mainRob.py
@@ -172,7 +172,25 @@ class MyRob(CRobLinkAngs):
 
         else: 
             print('Go')
-            self.driveMotors(0.1, 0.1)
+            action = self.safeguard()
+            self.driveMotors(action[0], action[1])
+        
+    def safeguard(self):
+        center_id = 0
+        left_id = 1
+        right_id = 2
+        back_id = 3
+
+        if self.measures.irSensor[center_id] > 5.0 \
+           or self.measures.irSensor[left_id]  > 5.0 \
+           or self.measures.irSensor[right_id] > 5.0 \
+           or self.measures.irSensor[back_id]  > 5.0:
+            return (-0.1, +0.1)
+        elif self.measures.irSensor[left_id]> 2.7:
+            return (0.1, 0.0)
+        elif self.measures.irSensor[right_id]> 2.7:
+            return (0.0, 0.1)
+        return (0.1, 0.1)
 
 
 class Map():
