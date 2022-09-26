0add78f74e79f1be70bd144cfcda4c7b72070858
Martinho Tavares <martinho.tavares@ua.pt>
2022-09-26 16:48:29 +0100

    Script to test robot mouse movement

---
+++ movRob.py
@@ -0,0 +1,36 @@
+import argparse
+from math import cos, sin, pi
+
+D = 1.0
+
+def driveMotors(inL, inR, outL_1, outR_1, theta_1, x_1, y_1):
+    outL = 0.5*inL + 0.5*outL_1
+    outR = 0.5*inR + 0.5*outR_1
+
+    lin = (outL + outR)/2
+    x = x_1 + lin*cos(theta_1)
+    y = y_1 + lin*sin(theta_1)
+
+    rot = (outR - outL)/D
+    theta = theta_1 + rot
+
+    return x, y, theta, outL, outR
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inL', type=float)
+    parser.add_argument('inR', type=float)
+    args = parser.parse_args()
+
+    inL, inR = args.inL, args.inR
+
+    theta = 0.0
+    outL = 0.0
+    outR = 0.0
+    x = 0.0
+    y = 0.0
+    while theta < pi/2:
+        x, y, theta, outL, outR = driveMotors(inL, inR, outL, outR, theta, x, y)
+        print(theta)
+
+    print('x:', x, 'y:', y)
